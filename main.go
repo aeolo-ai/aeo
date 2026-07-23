4205666e393037c30fc8cc6dea9fb321853bc70f
JiHyeok Kang <wlgur7238@snu.ac.kr>
2026-07-23 11:57:05 +0900

    chore(cli): release v2.3.6

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 	"time"
 )
 
-var version = "2.3.5"
+var version = "2.3.6"
 
 const segmentPauseDeprecatedMessage = "Tag-level pause is deprecated. Tags are metadata/filtering only. Use prompt status (tracked or untracked) to control measurement."
 
